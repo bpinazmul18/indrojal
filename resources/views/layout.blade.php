31f22a2e4d06dfe79c143fa67c4fa0e1520a30ea
Nazmul haque <bpinazmul18@gmail.com>
2022-12-12 10:07:50 +0600

    Fixed not found issues in layout.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,6 @@
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset('asset/icon/apple-touch-icon.png')}}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{asset('asset/icon/favicon-32x32.png')}}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('asset/icon/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{asset('/site.webmanifest')}}">
 
     <!-- Mobile Specific Metas-->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
